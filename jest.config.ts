fc69ef1323a6793a001250c58e97b4b61a6e1ad0
jsegomez <jsegomez06@gmail.com>
2025-04-25 23:54:00 -0600

    jest installed

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Config } from 'jest'; 
+
+export default {
+  moduleNameMapper: {
+    '@shared/(.*)': '<rootDir>/src/app/domains/shared/$1',
+    '@products/(.*)': '<rootDir>/src/app/domains/products/$1',
+    '@info/(.*)': '<rootDir>/src/app/domains/info/$1',
+    '@env/(.*)': '<rootDir>/src/environments/$1',
+  },
+  coverageDirectory: './coverage',
+  collectCoverageFrom: [
+    'src/app/**/*.ts',
+    '!<rootDir>/node_modules/',
+    '!<rootDir>/test/',
+  ]
+} satisfies Config;
+
+
+
